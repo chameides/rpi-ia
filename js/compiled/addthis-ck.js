4d20e4833911b7fe87fb785c6c08d30ef15b7d89
Michael Chameides <chamem@rpi.edu>
2013-10-03 14:53:28 -0400

    compiled addthis settings
    moved js to external document
    compiled with code kit
    linked index to externalized, compiled file

---
+++ js/compiled/addthis-ck.js
@@ -0,0 +1,1 @@
+addthis.layers({theme:"transparent",share:{position:"left",numPreferredServices:5},follow:{services:[{service:"facebook",id:"RPIAlumni"},{service:"twitter",id:"RPIAlumni"},{service:"google_follow",id:"107990642161097904060/posts"}]}});
