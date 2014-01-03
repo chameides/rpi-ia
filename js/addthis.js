573951be74f7ebd551832bbb278cc34cfd2f51ee
Michael Chameides <chamem@rpi.edu>
2014-01-03 13:21:40 -0500

    social media buttons for desktop breakpoint

--- js/addthis.js
+++ js/addthis.js
@@ -5,10 +5,15 @@ addthis.layers({
     'numPreferredServices' : 5
   },
   'follow' : {
+    'offset' : {'top':'.4rem'},
     'services' : [
       {'service': 'facebook', 'id': 'RPIAlumni'},
       {'service': 'twitter', 'id': 'RPIAlumni'},
-      {'service': 'google_follow', 'id': '107990642161097904060/posts'}
+      {'service': 'google_follow', 'id': '107990642161097904060/posts'},
+      {'service': 'pinterest', 'id': '107990642161097904060/posts'},
+      {'service': 'instagram', 'id': '107990642161097904060/posts'},
+      {'service': 'linkedin', 'id': '107990642161097904060/posts'}
     ]
+    
   },
 });
