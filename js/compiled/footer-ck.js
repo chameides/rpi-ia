983ec1c53eaab40a9408582e7b6917c460f4a5e5
Michael Chameides <chamem@rpi.edu>
2014-01-09 11:54:17 -0500

    adjusted follow buttons for desktop

--- js/compiled/footer-ck.js
+++ js/compiled/footer-ck.js
@@ -140,14 +140,21 @@ $(window).touchwipe({
 
 addthis.layers({
   'theme' : 'transparent',
+  'responsive' : {
+      'maxWidth' : '1023px',
+      'minWidth' : '0px'
+    },
+  //share buttons    
   'share' : {
     'position' : 'right',
     'services' : 'facebook,google_plusone_share,twitter,pinterest,linkedin,email,more',
     'offset' : {'bottom':'5.5rem'}
+
     //'numPreferredServices' : 5
   },
+  // follow buttons
   'follow' : {
-    'offset' : {'top':'1.5rem'},
+    'offset' : {'top':'1.7rem'},
     'services' : [
       {'service': 'facebook', 'id': 'RPIAlumni'},
       {'service': 'twitter', 'id': 'RPIAlumni'},
