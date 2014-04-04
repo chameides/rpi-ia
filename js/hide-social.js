d8fecb861f5e0ffbea3cd6e787ffd17b858b83d9
Michael Chameides <chamem@rpi.edu>
2014-04-04 16:52:22 -0400

    commenting hide social coding

--- js/hide-social.js
+++ js/hide-social.js
@@ -1,31 +1,27 @@
-//hide social media
+//when an input is focused, add class social-off
 $( "input" ).focus(function() {
 	$(document.body).toggleClass('social-off');
 });
-
+//when a textarea is focused, add class social-off
 $( "textarea" ).focus(function() {
 	$(document.body).toggleClass('social-off');
 });
-
+//when a select is focused, add class social-off
 $( "select" ).focus(function() {
 	$(document.body).toggleClass('social-off');
 });
 
 
-//return
 
+//when an input is stop being focused, remove class social-off
 $( "input" ).focusout(function() {
 	$(document.body).toggleClass('social-off');
 });
-
+//when a textarea is stop being focused, remove class social-off
 $( "textarea" ).focusout(function() {
 	$(document.body).toggleClass('social-off');
 });
-
+//when a select is stop being focused, remove class social-off
 $( "select" ).focusout(function() {
 	$(document.body).toggleClass('social-off');
 });
-
-
-
-//
