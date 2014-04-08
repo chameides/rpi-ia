60863999bdb1b897793aeb91384a699bb01be3ab
Michael Chameides <chamem@rpi.edu>
2014-04-08 09:13:55 -0400

    hide social: change toggle to add and remove
    in case there is a mix-up, this seems more bug proof

--- js/hide-social.js
+++ js/hide-social.js
@@ -1,27 +1,28 @@
 //when an input is focused, add class social-off
 $( "input" ).focus(function() {
-	$(document.body).toggleClass('social-off');
+	$(document.body).addClass('social-off');
 });
 //when a textarea is focused, add class social-off
 $( "textarea" ).focus(function() {
-	$(document.body).toggleClass('social-off');
+	$(document.body).addClass('social-off');
 });
 //when a select is focused, add class social-off
 $( "select" ).focus(function() {
-	$(document.body).toggleClass('social-off');
+	$(document.body).addClass('social-off');
 });
 
 
 
 //when an input is stop being focused, remove class social-off
 $( "input" ).focusout(function() {
-	$(document.body).toggleClass('social-off');
+	$(document.body).removeClass('social-off');
 });
 //when a textarea is stop being focused, remove class social-off
 $( "textarea" ).focusout(function() {
-	$(document.body).toggleClass('social-off');
+	$(document.body).removeClass('social-off');
 });
 //when a select is stop being focused, remove class social-off
 $( "select" ).focusout(function() {
-	$(document.body).toggleClass('social-off');
+	$(document.body).removeClass('social-off');
 });
+
